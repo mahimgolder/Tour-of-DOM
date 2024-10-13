f1517cfa6baed0086b57e9ae566a817b4882b0bb
mahimgolder <mahimgolder@gmail.com>
2024-10-12 19:01:46 -0600

    styled secound section

---
+++ js/first.js
@@ -0,0 +1,17 @@
+console.log("first file");
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+const allTitles = document.getElementsByClassName('section-title');
+console.log(allTitles);
+
+const sectionSection = document.getElementById('second-section');
+console.log(sectionSection);
+sectionSection.style.color = 'white';
+sectionSection.style.backgroundColor = 'tomato';
+
+
+// document.querrySelectorAll
+// document.querrySelector
+// prant node,child nood,children node,previous sevlings,next sevlings
